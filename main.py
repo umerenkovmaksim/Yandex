8e1c6e6877cd484edb276684a7de457c2499506a
IT-CUBE <umerenkovmaksim7@gmail.com>
2022-11-14 16:46:58 +0300

    Git и желтые окружности

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import uic
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.show_circles)
+        self.flag = False
+
+    def show_circles(self):
+        self.coords = random.randint(50, 400), random.randint(100, 430)
+        self.size = random.randint(20, 80)
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            painter = QPainter()
+            painter.begin(self)
+            self.draw_circle(painter)
+            painter.end()
+            self.flag = False
+
+    def draw_circle(self, painter):
+        painter.setBrush(Qt.yellow)
+        painter.drawEllipse(self.coords[0], self.coords[1], self.size, self.size)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
