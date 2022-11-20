431277eff76fb34637350f5e810f8abbfd7dbe58
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2022-11-20 15:40:35 +0300

    Капучино

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QTableWidgetItem
 from PyQt5 import uic, QtSql
+
+
+class Writer(QDialog):
+    def __init__(self, parent):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        inputs = [self.name, self.greed, self.type, self.description, self.cost, self.volume]
+        self.parent = parent
+        self.pushButton.clicked.connect(self.add)
+
+    def add(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        try:
+            cur.execute(f'''INSERT INTO Coffee(ID, coffee_grade, degree_of_roasting, ground_or_grains, description, cost,
+             volume) VALUES ({self.parent.model.rowCount() + 1}, '{self.name.text()}', '{self.greed.text()}', '{self.type.text()}', 
+             '{self.description.toPlainText()}', {float(self.cost.text())}, {int(self.volume.text())})''')
+            con.commit()
+        except ValueError:
+            self.parent.statusbar.showMessage('Ошибка ввода данных', 5000)
+        self.parent.update_table()
 
 
 class Example(QMainWindow):
@@ -9,9 +31,17 @@ class Example(QMainWindow):
         uic.loadUi('main.ui', self)
         self.model = QtSql.QSqlTableModel()
         self.tableView.setModel(self.model)
+        self.update_table()
+        self.pushButton.clicked.connect(self.write_data)
+
+    def update_table(self):
         self.model.setTable('coffee')
         self.model.select()
         self.tableView.resizeColumnsToContents()
+
+    def write_data(self):
+        write = Writer(self)
+        write.exec()
 
 
 def except_hook(cls, exception, traceback):
